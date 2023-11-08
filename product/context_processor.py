4c33c4719369e16dd942167c20a81cdd1ea71fac
meraj <merajpiri83@gmail.com>
2023-11-08 22:26:55 +0330

    modify cart

--- product/context_processor.py
+++ product/context_processor.py
@@ -3,8 +3,11 @@ def last_blogs(request):
     blogs = Blog.objects.all().order_by("-created")[0:5]
     return {"last_blogs" : blogs}
 def total_products(request):
-    cart = request.user.carts.filter(is_open=True,is_paid=False).first()
-    total = 0
-    for cart_product in cart.cart_products.all() :
-        total += cart_product.count
-    return {"total_products":total}
+    if request.user.is_authenticated :
+        cart = request.user.carts.filter(is_open=True, is_paid=False).first()
+        if cart :
+            total = 0
+            for cart_product in cart.cart_products.all():
+                total += cart_product.count
+            return {"total_products": total}
+    return []
